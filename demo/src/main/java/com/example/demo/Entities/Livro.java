3a70256f1828866f63d02b8088b3ed451253f44c
WesleyWaterkemper <oobuyuki@gmail.com>
2025-05-12 16:07:32 -0300

    Criando os atributos da Entidade Livro

--- demo/src/main/java/com/example/demo/Entities/Livro.java
+++ demo/src/main/java/com/example/demo/Entities/Livro.java
@@ -1,5 +1,37 @@
 package com.example.demo.Entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "livros")
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
 public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "titulo")
+    private String titulo;
+
+    @Column(nullable = false, name = "autor")
+    private String autor;
+
+    @Column(nullable = false, unique = true, name = "isbn")
+    private  String isbn;
     
+    @Column(nullable = false, name = "quantidade")
+    private int quantidade;
+
+    @Column(nullable = false, name = "categoria")
+    private String categoria;
 }
