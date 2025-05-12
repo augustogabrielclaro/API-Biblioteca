ceff1b3b644ba17d908d1b0addccf1bacac06364
WesleyWaterkemper <oobuyuki@gmail.com>
2025-05-12 16:30:19 -0300

    Criação da classe LivroService. Criado os métodos para salvar, listar todos, buscar por id e atualizar

---
+++ demo/src/main/java/com/example/demo/service/LivroService.java
@@ -0,0 +1,46 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Entities.Livro;
+import com.example.demo.dto.LivroDTO;
+import com.example.demo.mapper.ILivroMapper;
+import com.example.demo.repository.ILivroRepository;
+
+@Service
+public class LivroService {
+    @Autowired
+    private ILivroRepository livroRepository;
+    @Autowired
+    private ILivroMapper livroMapper;
+
+    public LivroDTO salvar(LivroDTO livroDTO) {
+        Livro livro = livroMapper.toEntity(livroDTO);
+
+        return livroMapper.toDTO(livroRepository.save(livro));
+    }
+
+    public List<LivroDTO> listarTodos() {
+        return livroMapper.toDTOList(livroRepository.findAll());
+    }
+
+    public Optional<LivroDTO> buscarPorId(Long id) {
+        return livroRepository.findById(id).map(livroMapper::toDTO);
+    }
+
+    public LivroDTO atualizar(Long id, LivroDTO livroDTO) {
+        Livro livroExistente = livroRepository.findById(id).orElseThrow(() -> new RuntimeException("Livro não encontrado"));
+        
+        livroExistente.setTitulo(livroDTO.getTitulo());
+        livroExistente.setAutor(livroDTO.getAutor());
+        livroExistente.setIsbn(livroDTO.getIsbn());
+        livroExistente.setQuantidade(livroDTO.getQuantidade());
+        livroExistente.setCategoria(livroDTO.getCategoria());
+
+        return livroMapper.toDTO(livroRepository.save(livroExistente));
+    }
+}
