b5df7bdbcc8867bced5fc0352b8ae260c95a49f7
WesleyWaterkemper <oobuyuki@gmail.com>
2025-05-12 16:12:16 -0300

    Criação dos atributos da classe LivroDTO

--- demo/src/main/java/com/example/demo/dto/LivroDTO.java
+++ demo/src/main/java/com/example/demo/dto/LivroDTO.java
@@ -1,5 +1,26 @@
 package com.example.demo.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class LivroDTO {
-    
+    private Long id;
+
+    @NotBlank(message = "Titulo do Livro é obrigatório")
+    private String titulo;
+
+    @NotBlank(message = "Autor do Livro é obrigatório")
+    private String autor;
+
+    @NotBlank(message = "ISBN do Livro é obrigatório")
+    private String isbn;
+
+    @NotBlank(message = "Quantidade do Livro é obrigatório")
+    private int quantidade;
+
+    @NotBlank(message = "Categoria do Livro é obrigatório")
+    private String categoria;
 }
