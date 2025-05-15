6843986445705a52e78012f885552cf6762d1ead
Hugo <hugocesar.fm@gmail.com>
2025-05-14 21:26:53 -0300

    Entities, Repository e DTO

--- demo/src/main/java/com/example/demo/dto/ClienteDTO.java
+++ demo/src/main/java/com/example/demo/dto/ClienteDTO.java
@@ -1,5 +1,48 @@
 package com.example.demo.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class ClienteDTO {
-    
+
+    private long id;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String nome;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String telefone;
+
+    @NotBlank
+    private String endereco;
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public String getTelefone() { return telefone; }
+
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    public String getEndereco() { return endereco; }
+
+    public void setEndereco(String endereco) { this.endereco = endereco; }
 }
+
+    
+    
+
