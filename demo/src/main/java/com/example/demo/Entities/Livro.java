8c62bc9f75b4803888dbe8bdec7d05d8cc240df4
WesleyWaterkemper <oobuyuki@gmail.com>
2025-05-22 19:48:26 -0300

    Ajustando anotações

--- demo/src/main/java/com/example/demo/Entities/Livro.java
+++ demo/src/main/java/com/example/demo/Entities/Livro.java
@@ -7,14 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
+@Data
 @Table(name = "livros")
 @NoArgsConstructor @AllArgsConstructor
-@Getter @Setter
 public class Livro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
