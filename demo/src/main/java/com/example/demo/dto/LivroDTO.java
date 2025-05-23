727617c0b5954a7d7bb54b3720689988f74dd722
WesleyWaterkemper <oobuyuki@gmail.com>
2025-05-22 22:09:29 -0300

    Criação de validação com regex do campo isbn

--- demo/src/main/java/com/example/demo/dto/LivroDTO.java
+++ demo/src/main/java/com/example/demo/dto/LivroDTO.java
@@ -1,6 +1,8 @@
 package com.example.demo.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,9 +18,13 @@ public class LivroDTO {
     private String autor;
 
     @NotBlank(message = "ISBN do Livro é obrigatório")
+    @Pattern(
+        regexp = "^(978-65-(\\d{1}-\\d{6}|\\d{2}-\\d{5}|\\d{3}-\\d{4})-\\d{1})$",
+        message = "O código isbn deve conter o formato ISBN-13"
+    )
     private String isbn;
 
-    @NotBlank(message = "Quantidade do Livro é obrigatório")
+    @Min(value = 0, message = "Quantidade do Livro é obrigatório")
     private int quantidade;
 
     @NotBlank(message = "Categoria do Livro é obrigatório")
