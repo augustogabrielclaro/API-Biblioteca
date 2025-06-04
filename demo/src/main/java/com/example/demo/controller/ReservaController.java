0c396b60de9f9ae2f786bbbbef4d5f4f7d443367
Pedro Gerhard <pedrogerhardc@gmail.com>
2025-06-04 15:11:48 -0300

    feat: Adiciona o metodo put na reserva

--- demo/src/main/java/com/example/demo/controller/ReservaController.java
+++ demo/src/main/java/com/example/demo/controller/ReservaController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +21,6 @@ import com.example.demo.service.Utils.ApiResponse;
 import com.example.demo.service.Utils.ErrorResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
@@ -71,5 +72,29 @@ public class ReservaController {
     public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
         reservaService.deletar(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @Operation(summary = "Atualiza uma reserva por completo", description = "Método para atualizar uma reserva do banco de dados por completo")
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<ReservaDTO>> atualizarReserva(@PathVariable Long id, @Valid @RequestBody ReservaDTO reservaDTO) {
+        try {
+            ReservaDTO savedReserva = reservaService.sobrescreverReserva(id, reservaDTO);
+
+            ApiResponse<ReservaDTO> response = new ApiResponse<>(savedReserva);
+
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+        } catch (IllegalArgumentException e) {
+            ErrorResponse errorResponse = new ErrorResponse("Argumento inválido!", e.getMessage());
+
+            ApiResponse<ReservaDTO> response = new ApiResponse<>(errorResponse);
+
+            return ResponseEntity.badRequest().body(response);
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse("Erro interno", e.getMessage());
+
+            ApiResponse<ReservaDTO> response = new ApiResponse<>(errorResponse);
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
     }
 }
--- demo/src/main/java/com/example/demo/service/ReservaService.java
+++ demo/src/main/java/com/example/demo/service/ReservaService.java
@@ -9,9 +9,7 @@ import org.springframework.stereotype.Service;
 import com.example.demo.Entities.Cliente;
 import com.example.demo.Entities.Livro;
 import com.example.demo.Entities.Reserva;
-import com.example.demo.dto.MultaDTO;
 import com.example.demo.dto.ReservaDTO;
-import com.example.demo.mapper.IClienteMapper;
 import com.example.demo.mapper.IReservaMapper;
 import com.example.demo.repository.IClienteRepository;
 import com.example.demo.repository.ILivroRepository;
@@ -32,14 +30,17 @@ public class ReservaService {
     @Autowired
     private ILivroRepository livroRepository;
 
+    // GET
     public List<ReservaDTO> listarTodos() {
         return reservaMapper.toDTOList(reservaRepository.findAll());
     }
 
+    // GET
     public Optional<ReservaDTO> buscarPorId(Long id) {
         return reservaRepository.findById(id).map(reservaMapper::toDTO);
     }
 
+    // POST
     public ReservaDTO salvar(ReservaDTO reservaDTO) {
         Cliente cliente = clienteRepository.findById(reservaDTO.getCliente_id())
         .orElseThrow(()-> new IllegalArgumentException("Cliente não encontrado!"));
@@ -57,8 +58,32 @@ public class ReservaService {
         return reservaMapper.toDTO(reservaRepository.save(reserva));
     }
 
+    // DELETE
     public void deletar(Long id) {
+        if (!reservaRepository.existsById(id)) {
+            throw new IllegalArgumentException("Reserva com o ID " + id + " não encontrado!");
+        }
+        
         reservaRepository.deleteById(id);
+    }
 
+    // PUT
+    public ReservaDTO sobrescreverReserva(Long id, ReservaDTO reservaDTO) {
+        Reserva reserva = reservaRepository.findById(id).
+                                orElseThrow(() -> new IllegalArgumentException("Reserva não encontrada!"));
+        
+        Cliente cliente = clienteRepository.findById(reservaDTO.getCliente_id())
+        .orElseThrow(()-> new IllegalArgumentException("Cliente não encontrado!"));
+
+        Livro livro = livroRepository.findById(reservaDTO.getLivro_id())
+        .orElseThrow(()-> new IllegalArgumentException("Livro não encontrado!"));
+        
+        reserva.setCliente(cliente);
+        reserva.setLivro(livro);
+        reserva.setDataReserva(reservaDTO.getDataReserva());
+        reserva.setStatus(reservaDTO.getStatus());
+
+        return reservaMapper.toDTO(reservaRepository.save(reserva));
+        
     }
 }
