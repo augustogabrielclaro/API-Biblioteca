8012b8cf15b154352964af14840dff2b9d0b92c2
augustogabrielclaro <augustogabrielclaro@hotmail.com>
2025-06-04 16:09:28 -0300

    Fix: Description and title of SwaggerUI

--- demo/src/main/java/com/example/demo/config/SwaggerConfig.java
+++ demo/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,12 +1,8 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-
 import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -27,9 +23,9 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
     public OpenApiCustomizer customOpenApi() {
         return openApi -> {
-            openApi.getInfo().setTitle("Seminário 3 ADS/ESW"); // Renomeia o título
+            openApi.getInfo().setTitle("Seminário 3 ADS"); // Renomeia o título
             openApi.getInfo().setVersion("1.0.0"); // Define a versão
-            openApi.getInfo().setDescription("API de gerenciamento de usuarios."); // Define a descrição
+            openApi.getInfo().setDescription("API de gerenciamento de sistema de biblioteca."); // Define a descrição
         };
     }
 }
