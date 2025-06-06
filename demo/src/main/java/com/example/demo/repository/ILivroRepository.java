df1d6811c735d78390940a01ee23c6c9b72c854f
WesleyWaterkemper <oobuyuki@gmail.com>
2025-06-05 22:03:26 -0300

    Remoção do campo quantidade da Query

--- demo/src/main/java/com/example/demo/repository/ILivroRepository.java
+++ demo/src/main/java/com/example/demo/repository/ILivroRepository.java
@@ -11,7 +11,7 @@ import com.example.demo.Entities.Livro;
 @Repository
 public interface ILivroRepository extends JpaRepository <Livro, Long>{
     @Query(
-        value = "SELECT L.id, L.titulo, L.autor, L.isbn, L.quantidade, L.categoria " + 
+        value = "SELECT L.id, L.titulo, L.autor, L.isbn, L.categoria " + 
         "FROM livros L LEFT JOIN emprestimos E ON L.id = E.livro_id " + 
         "WHERE STATUS = 'Concluido' OR E.id is null",
         nativeQuery = true
